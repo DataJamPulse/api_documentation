a23dbea943fe7b62d4c532619d6528a083295db8
Arran Javed <jav@Mac.lan>
2026-01-22 15:12:21 -0800

    Test: send to arran@data-jam.com directly

--- netlify/functions/send-notification.js
+++ netlify/functions/send-notification.js
@@ -94,8 +94,8 @@ exports.handler = async (event) => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                from: 'API ACCESS ALERT <notifications@send.data-jam.com>',
-                to: ['Team@data-jam.com'],
+                from: 'API ACCESS ALERT <notifications@data-jam.com>',
+                to: ['arran@data-jam.com'],
                 subject: `New API Docs Visitor: ${email}`,
                 html: emailBody
             })
